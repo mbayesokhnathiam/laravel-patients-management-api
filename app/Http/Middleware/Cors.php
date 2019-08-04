4128a6e1e49d8ed9aa8593842d06b19b86984cc6
abdoulaye bacary <abdoulayebacary@gmail.com>
2019-08-04 16:03:57 +0000

    cors resolution bizar

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -8,14 +8,14 @@ class Cors
 {
     /**
      * Handle an incoming request.
-     *
+     * ->header('Access-Control-Allow-Origin', '*')
      * @param  \Illuminate\Http\Request  $request
      * @param  \Closure  $next
      * @return mixed
      */
     public function handle($request, Closure $next)
     {
-        return $next($request)->header('Access-Control-Allow-Origin', '*')
+        return $next($request)
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE, OPTIONS')
             ->header('Access-Control-Allow-Headers', 'Content-Type, Authorization, X-Requested-With, X-XSRF-TOKEN');
     }
